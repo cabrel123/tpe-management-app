2ef072986e697fd6681ba9545a1f01cb86fbc25d
cabrel123 <monkamcabrel11@gmail.com>
2023-09-11 18:58:04 +0100

    update

--- tpe-api/server.js
+++ tpe-api/server.js
@@ -27,6 +27,59 @@ const userSchema = new mongoose.Schema({
   username: String,
   password: String,
 });
+
+const User = mongoose.model('User', userSchema);
+
+// Clé secrète pour signer les tokens JWT (gardez ceci secret en production)
+const secretKey = 'TpeApp2023';
+// Route de connexion utilisateur
+app.post('/login', async (req, res) => {
+	try {
+	  const { username, password } = req.body;
+  
+	  // Recherche de l'utilisateur dans la base de données
+	  const user = await User.findOne({ username });
+  
+	  if (!user) {
+		return res.status(401).json({ message: 'Nom d\'utilisateur ou mot de passe incorrect' });
+	  }
+  
+	  // Vérification du mot de passe
+	  const isPasswordValid = await bcrypt.compare(password, user.password);
+  
+	  if (!isPasswordValid) {
+		return res.status(401).json({ message: 'Nom d\'utilisateur ou mot de passe incorrect' });
+	  }
+  
+	  // Création d'un token JWT
+	  const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+  
+	  res.json({ token });
+	} catch (error) {
+	  console.error(error);
+	  res.status(500).json({ message: 'Erreur lors de la connexion' });
+	}
+  });
+  
+  // Middleware pour vérifier l'authentification JWT
+  function verifyToken(req, res, next) {
+	const token = req.headers.authorization;
+  
+	if (!token) {
+	  return res.status(403).json({ message: 'Token manquant' });
+	}
+  
+	// Vérifie le token JWT
+	jwt.verify(token, secretKey, (err, decoded) => {
+	  if (err) {
+		return res.status(401).json({ message: 'Token invalide' });
+	  }
+  
+	  req.username = decoded.username;
+	  next();
+	});
+  }
+  
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
